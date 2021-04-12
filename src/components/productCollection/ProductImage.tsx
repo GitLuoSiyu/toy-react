4fe67a07c3c7861328a8580476715dfbdcd240f1
luosiyu <1514813390@qq.com>
2021-04-13 00:46:10 +0800

    配置redux

--- src/components/productCollection/ProductImage.tsx
+++ src/components/productCollection/ProductImage.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Image, Typography } from "antd";
-import { withRouter, RouteComponentProps } from "react-router-dom";
+import { withRouter, RouteComponentProps, Link } from "react-router-dom";
 
 interface PropsType extends RouteComponentProps {
-    id: string | number;
-    size: "large" | "small";
-    imageSrc: string;
-    price: number | string;
-    title: string;
+  id: string | number;
+  size: "large" | "small";
+  imageSrc: string;
+  price: number | string;
+  title: string;
 }
 
 const ProductImageComponent: React.FC<PropsType> = ({
@@ -18,25 +18,23 @@ const ProductImageComponent: React.FC<PropsType> = ({
   title,
   history,
   location,
-  match
+  match,
 }) => {
-    return (
-      <div onClick={() => history.push(`detail/${id}`) }>
-        {size == "large" ? (
-          <Image src={imageSrc} height={285} width={490} />
-        ) : (
-          <Image src={imageSrc} height={120} width={240} />
-        )}
-        <div>
-          <Typography.Text type="secondary">
-            {title.slice(0, 25)}
-          </Typography.Text>
-          <Typography.Text type="danger" strong>
-            ¥ {price} 起
-          </Typography.Text>
-        </div>
+  return (
+    <Link to={`detail/${id}`}>
+      {size == "large" ? (
+        <Image src={imageSrc} height={285} width={490} />
+      ) : (
+        <Image src={imageSrc} height={120} width={240} />
+      )}
+      <div>
+        <Typography.Text type="secondary">{title.slice(0, 25)}</Typography.Text>
+        <Typography.Text type="danger" strong>
+          ¥ {price} 起
+        </Typography.Text>
       </div>
-    );
-}
+    </Link>
+  );
+};
 
-export const ProductImage = withRouter(ProductImageComponent)
+export const ProductImage = withRouter(ProductImageComponent);
