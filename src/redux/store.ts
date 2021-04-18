a170f9608d4911fafff11b17a1b4d5037669e22a
GitLuosiyu <1514813390@qq.com>
2021-04-18 23:10:53 +0800

    add redux chunk

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import languageReducer from "./language/languageReducer";
+import recommendProductsReducer from "./recommendProducts/recommendProductsReducer";
+// import thunk from "redux-thunk";
 
+const rootReducer = combineReducers({
+    language: languageReducer,
+    recommendProducts: recommendProductsReducer
+})
 const store = createStore(languageReducer);
 
 export type RootState = ReturnType<typeof store.getState>
-
 export default store;
